cf8fa933f634aab76d6cf902050fcbc4dbcd8549
amiloszewicz <miloszewicz.adam@gmail.com>
2023-07-10 17:29:12 +0200

    refactor (router): set app, home routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.routes').then((m) => m.routes),
   },
 ];
--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomePage } from './home.page';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomePage,
+    loadComponent: () => import('./home.page').then((m) => m.HomePage),
   },
 ];
