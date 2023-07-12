b84313c0b1ce241f7cb8a9d446747a001e998c28
amiloszewicz <miloszewicz.adam@gmail.com>
2023-07-12 17:01:09 +0200

    refactor (service): add item to checklist and getById

--- src/app/checklist/data-access/checklist-item.service.ts
+++ src/app/checklist/data-access/checklist-item.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import {
+  AddChecklistItem,
+  ChecklistItem,
+} from 'src/app/shared/interfaces/chhcecklist-item';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChecklistItemService {
+  private checklistItems$ = new BehaviorSubject<ChecklistItem[]>([]);
+
+  getItemsByChecklistId(checklistId: string) {
+    return this.checklistItems$.pipe(
+      map((items) => items.filter((item) => item.checklistId === checklistId))
+    );
+  }
+
+  add(item: AddChecklistItem, checklistId: string) {
+    const newItem = {
+      id: Date.now().toString(),
+      checklistId,
+      checked: false,
+      ...item,
+    };
+
+    this.checklistItems$.next([...this.checklistItems$.value, newItem]);
+  }
+
   constructor() {}
 }
