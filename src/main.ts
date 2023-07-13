b9a97816c899c7b31d5601dfde55fe65008fc675
amiloszewicz <miloszewicz.adam@gmail.com>
2023-07-13 20:18:23 +0200

    feat (module): IonicStorageModule with DB, localstorage drivers

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { Drivers } from '@ionic/storage';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
 
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 import { environment } from './environments/environment';
 
 if (environment.production) {
@@ -14,7 +17,17 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    importProvidersFrom(IonicModule.forRoot({})),
+    importProvidersFrom(
+      IonicModule.forRoot({}),
+      IonicStorageModule.forRoot({
+        driverOrder: [
+          // eslint-disable-next-line no-underscore-dangle
+          CordovaSQLiteDriver._driver,
+          Drivers.IndexedDB,
+          Drivers.LocalStorage,
+        ],
+      })
+    ),
     provideRouter(routes),
   ],
 });
