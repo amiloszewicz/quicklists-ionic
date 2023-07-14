aab015094f0d18bc38e25c7961e177f2cc3a0ca1
amiloszewicz <miloszewicz.adam@gmail.com>
2023-07-14 11:22:10 +0200

    feat (observable): priv ionicStorage pipe

--- src/app/shared/data-access/storage.service.ts
+++ src/app/shared/data-access/storage.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { from, shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StorageService {
-  constructor() {}
+  private storage$ = from(this.ionicStorage.create()).pipe(shareReplay(1));
+
+  constructor(private ionicStorage: Storage) {}
 }
