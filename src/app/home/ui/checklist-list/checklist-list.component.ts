cbf3d8dfa4fe3eeba7c8731786b228c54e02751b
amiloszewicz <miloszewicz.adam@gmail.com>
2023-07-18 18:28:44 +0200

    feat (element): ion reorder checklist group

--- src/app/home/ui/checklist-list/checklist-list.component.ts
+++ src/app/home/ui/checklist-list/checklist-list.component.ts
@@ -8,7 +8,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { IonList, IonicModule } from '@ionic/angular';
+import { IonList, IonicModule, ItemReorderEventDetail } from '@ionic/angular';
 import { Checklist } from 'src/app/shared/interfaces/checklist';
 
 @Component({
@@ -32,6 +32,17 @@ export class ChecklistListComponent {
 
   trackByFn(index: number, checklist: Checklist) {
     return checklist.id;
+  }
+
+  handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
   }
 
   constructor() {}
